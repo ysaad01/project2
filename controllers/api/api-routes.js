6c792e50e052190c919ff5364c814bc0e92491a1
Michael Cota <mikecotahf@gmail.com>
2022-05-12 20:55:18 -0400

    finished routes for login, logout, and signup

--- controllers/api/api-routes.js
+++ controllers/api/api-routes.js
@@ -1,24 +1,28 @@
-module.exports = function (app) {
-    // Route for logging in user
-    app.post("/api/login", passport.authenticate("local"), (req, res) => {
-        res.json({
-            email: req.user.email,
-            id: req.user.id
-        });
-    });
+const db = require("../models");
 
-    app.post("/api/signup", (req, res) => {
-        db.User.create({
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            email: req.body.email,
-            password: req.body.password
+// Login api routes
+app.post("/api/login", passport.authenticate("local"), function (req, res) {
+    res.json("/user/profile");
+});
+
+// Route for signing up user
+app.post("/api/signup", (req, res) => {
+    db.User.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(() => {
+            res.redirect(307, "/api/login");
         })
-            .then(() => {
-                res.redirect(307, "/api/login");
-            })
-            .catch(err => {
-                res.status(401).json(err);
-            });
-    });
-}
+        .catch(err => {
+            res.status(401).json(err);
+        });
+});
+
+// Route for logging user out
+app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+});
