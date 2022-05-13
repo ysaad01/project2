4f4e11dd300f0e012d95591e0d5cccc0df1e017c
Michael Cota <mikecotahf@gmail.com>
2022-05-12 21:29:23 -0400

    updated index.js for api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 
+const apiRoutes = require("./api-routes");
 const userRoutes = require("./user-routes");
 const petsRoutes = require("./pets-routes");
 
+router.use("/api", apiRoutes);
 router.use("/users", userRoutes);
 router.use("/pets", petsRoutes);
 
