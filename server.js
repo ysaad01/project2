57a7fff1e8b2071e2123eec2dd742fbc8a241041
ysaad01 <ysaad2325@gmail.com>
2022-05-15 19:42:09 -0400

    commented out login feature to restore connection

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 const path = require("path");
+const sequelize = require("./config/connection");
 const express = require("express");
 const exphbs = require("express-handlebars");
-// const SequelizeStore = require("connect-session-sequelize")(session_Store);
 const routes = require("./controllers");
-const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
+
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const sess = {
-//   secret: "Super secret secret",
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize,
-//   }),
-// };
+const sess = {
+  secret: process.env.DB_SESSION_SECRET,
+  cookie: { maxAge: 10800000 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-// app.use(session(sess));
+app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 
@@ -27,7 +29,7 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
