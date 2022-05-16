5ac802e5a8b8611a37501004fb7376c8a54b5c29
ysaad01 <ysaad2325@gmail.com>
2022-05-15 21:46:59 -0400

    moved session secret to .env file

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
 const path = require("path");
+const sequelize = require("./config/connection");
 const express = require("express");
 const exphbs = require("express-handlebars");
-// const SequelizeStore = require("connect-session-sequelize")(session_Store);
 const routes = require("./controllers");
-const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const sess = {
-//   secret: "Super secret secret",
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize,
-//   }),
-// };
-
-// app.use(session(sess));
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: process.env.DB_SESSION_SECRET,
+  cookie: { maxAge: 10800000 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
