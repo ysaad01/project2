3769f782c1836d1d1a52a46fbb1ff51454a8cb09
ysaad01 <ysaad2325@gmail.com>
2022-05-15 21:35:24 -0400

    commented last pull request to restore connection; downloaded session dependencies

--- controllers/api/api-routes.js
+++ controllers/api/api-routes.js
@@ -1,28 +1,28 @@
-const db = require("../models");
+// const db = require("../models");
 
-// Login api routes
-app.post("/api/login", passport.authenticate("local"), function (req, res) {
-    res.json("/user/profile");
-});
+// // Login api routes
+// app.post("/api/login", passport.authenticate("local"), function (req, res) {
+//     res.json("/user/profile");
+// });
 
-// Route for signing up user
-app.post("/api/signup", (req, res) => {
-    db.User.create({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        password: req.body.password
-    })
-        .then(() => {
-            res.redirect(307, "/api/login");
-        })
-        .catch(err => {
-            res.status(401).json(err);
-        });
-});
+// // Route for signing up user
+// app.post("/api/signup", (req, res) => {
+//     db.User.create({
+//         first_name: req.body.first_name,
+//         last_name: req.body.last_name,
+//         email: req.body.email,
+//         password: req.body.password
+//     })
+//         .then(() => {
+//             res.redirect(307, "/api/login");
+//         })
+//         .catch(err => {
+//             res.status(401).json(err);
+//         });
+// });
 
-// Route for logging user out
-app.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/");
-});
+// // Route for logging user out
+// app.get("/logout", (req, res) => {
+//     req.logout();
+//     res.redirect("/");
+// });
--- public/js/createaccount.js
+++ public/js/createaccount.js
@@ -1,50 +1,50 @@
-$(document).ready(() => {
-    // References for form and input
-    const signUpForm = $("form.signup");
-    const firstNameInput = $("input#first-name-input");
-    const lastNameInput = $("input#last-name-input");
-    const emailInput = $("input#email-input");
-    const passwordInput = $("input#password-input");
+// $(document).ready(() => {
+//     // References for form and input
+//     const signUpForm = $("form.signup");
+//     const firstNameInput = $("input#first-name-input");
+//     const lastNameInput = $("input#last-name-input");
+//     const emailInput = $("input#email-input");
+//     const passwordInput = $("input#password-input");
 
-    // Check that form is not blank
-    signUpForm.on("submit", event => {
-        event.preventDefault();
-        const userData = {
-            first_name: firstNameInput.val().trim(),
-            last_name: lastNameInput.val().trim(),
-            email: emailInput.val().trim(),
-            password: passwordInput.val().trim()
-        };
+//     // Check that form is not blank
+//     signUpForm.on("submit", event => {
+//         event.preventDefault();
+//         const userData = {
+//             first_name: firstNameInput.val().trim(),
+//             last_name: lastNameInput.val().trim(),
+//             email: emailInput.val().trim(),
+//             password: passwordInput.val().trim()
+//         };
 
-        if (!userData.first_name || !userData.last_name || !userData.email || !userData.password) {
-            return;
-        }
-        // If we have an email and password, run the signUpUser function
-        signUpUser(userData.first_name, userData.last_name, userData.email, userData.password);
-        firstNameInput.val("");
-        lastNameInput.val("");
-        emailInput.val("");
-        passwordInput.val("");
-    });
+//         if (!userData.first_name || !userData.last_name || !userData.email || !userData.password) {
+//             return;
+//         }
+//         // If we have an email and password, run the signUpUser function
+//         signUpUser(userData.first_name, userData.last_name, userData.email, userData.password);
+//         firstNameInput.val("");
+//         lastNameInput.val("");
+//         emailInput.val("");
+//         passwordInput.val("");
+//     });
 
-    // Does a post to the signup route. If successful, we are redirected to the members page
-    // Otherwise we log any errors
-    function signUpUser(first_name, last_name, email, password) {
-        $.post("/api/signup", {
-            first_name: first_name,
-            last_name: last_name,
-            email: email,
-            password: password
-        })
-            .then(() => {
-                window.location.replace("/members");
-                // If there's an error, handle it by throwing up a bootstrap alert
-            })
-            .catch(handleLoginErr);
-    }
+//     // Does a post to the signup route. If successful, we are redirected to the members page
+//     // Otherwise we log any errors
+//     function signUpUser(first_name, last_name, email, password) {
+//         $.post("/api/signup", {
+//             first_name: first_name,
+//             last_name: last_name,
+//             email: email,
+//             password: password
+//         })
+//             .then(() => {
+//                 window.location.replace("/members");
+//                 // If there's an error, handle it by throwing up a bootstrap alert
+//             })
+//             .catch(handleLoginErr);
+//     }
 
-    function handleLoginErr(err) {
-        $("#alert .msg").text(err.responseJSON);
-        $("#alert").fadeIn(500);
-    }
-});
+//     function handleLoginErr(err) {
+//         $("#alert .msg").text(err.responseJSON);
+//         $("#alert").fadeIn(500);
+//     }
+// });
