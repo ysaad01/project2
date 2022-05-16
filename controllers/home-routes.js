e8a08bb663d50201f4bb769478a6f29fdaf60cdc
ysaad01 <ysaad2325@gmail.com>
2022-05-15 20:40:25 -0400

    added signup page for users to create an account

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,9 +2,34 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { User, Pets } = require("../models");
 
+// router.get("/", (req, res) => {
+//   Pets.findAll({
+//     attributes: ["dog_name", "owner_id"],
+//     include: [
+//       {
+//         model: User,
+//         attributes: ["username"],
+//       },
+//     ],
+//   })
+//     .then((dbPetData) => {
+//       const pets = dbPetData.map((post) => post.get({ plain: true }));
+//       res.render("homepage", { pets });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
 // Render the login page
 router.get("/login", (req, res) => {
   res.render("login");
+});
+
+// Render the signup page
+router.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
 module.exports = router;
