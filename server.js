1b8c4456f952f0568d00a030f34da5c73d8d6aae
ysaad01 <ysaad2325@gmail.com>
2022-05-17 19:39:26 -0400

    added session

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
+const session = require("express-session");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
