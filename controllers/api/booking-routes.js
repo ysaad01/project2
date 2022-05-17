32820ad15b3d9dbe84139fbf3c3c0a8ad9c11f16
Michael Cota <mikecotahf@gmail.com>
2022-05-17 17:00:12 -0400

    adding bookingroutes to index.js and initial setup of booking-routes

--- controllers/api/booking-routes.js
+++ controllers/api/booking-routes.js
@@ -3,5 +3,30 @@ const passport = require("passport");
 const querystring = require("querystring");
 
 const Booking = require("../models/Booking");
-const Accommodation = require("../models/Accommodation");
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
