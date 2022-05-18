7e82def2a7d04a8cf3b784f6003b1f14fadfa8c3
Michael Cota <mikecotahf@gmail.com>
2022-05-17 20:05:56 -0400

    corrected folder error in booking-routes

--- controllers/api/booking-routes.js
+++ controllers/api/booking-routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const passport = require("passport");
 const querystring = require("querystring");
 
-const Booking = require("../models/Booking");
+const Booking = require("../../models/Booking");
 
 
 // Create a new booking
