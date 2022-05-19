c5515939bbab77b914bc7d4bdbde2d595b5d0cc0
Michael Cota <mikecotahf@gmail.com>
2022-05-19 18:40:53 -0400

    got create a new booking working

--- controllers/api/booking-routes.js
+++ controllers/api/booking-routes.js
@@ -13,13 +13,44 @@ router.get("/", (req, res) => {
     });
 });
 
-// Create a new booking
-router.post("/", (req, res, next) => {
-  Booking.create(req.body)
-    .then((booking) => {
-      res.send(booking);
+// get dog by id
+router.get("/:id", (req, res) => {
+  Booking.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((bookingData) => {
+      if (!bookingData) {
+        res.status(404).json({ message: "No Booking was found with that ID!" });
+        return;
+      }
+      res.json(bookingData);
     })
-    .catch(next);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// Create a new booking
+router.post("/", (req, res) => {
+  // create a new PET
+  Booking.create({
+    owner_id: req.body.owner_id,
+    pets_id: req.body.pets_id,
+    dog_name: req.body.dog_name,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+  })
+    .then((bookingData) => {
+      // instead of sending back pet data might want to try res.redirect(/dashboard)
+      res.send(bookingData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // Update a booking
@@ -32,10 +63,27 @@ router.put("/:id", (req, res, next) => {
 });
 
 // Delete a booking
-router.delete("/:id", (req, res, next) => {
-  Booking.findByIdAndRemove({ _id: req.params.id }).then((booking) => {
-    res.send(booking);
-  });
+// router.delete("/:id", (req, res, next) => {
+//   Booking.findByIdAndRemove({ _id: req.params.id }).then((booking) => {
+//     res.send(booking);
+//   });
+// });
+
+router.delete("/:id", async (req, res) => {
+  // delete a user by its `id` value
+  try {
+    const deleteBooking = await Booking.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(201).json({
+      message: "Booking has been deleted!",
+      data: deleteBooking,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
