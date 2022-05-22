eb6f07997e4699dfd1a566c302b74cf5c5ecd96c
ysaad01 <ysaad2325@gmail.com>
2022-05-22 03:16:07 -0400

    updated model/index.js and dashboard.handlebars files to try and fix heroku error after deleting booking

--- models/index.js
+++ models/index.js
@@ -17,21 +17,27 @@ Pets.belongsTo(User, {
 
 Pets.hasMany(Booking, {
   foreignKey: "pets_id",
+  onDelete: "SET NULL",
+  hooks: true,
 });
 
 Booking.belongsTo(Pets, {
   foreignKey: "pets_id",
   onDelete: "SET NULL",
+  hooks: true,
 });
 
 // user to appt association(user has many appts but appt belongs to one user)
 User.hasMany(Booking, {
   foreignKey: "owner_id",
+  onDelete: "SET NULL",
+  hooks: true,
 });
 
 Booking.belongsTo(User, {
   foreignKey: "owner_id",
   onDelete: "SET NULL",
+  hooks: true,
 });
 
 module.exports = { User, Pets, Booking };
